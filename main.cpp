5f2766a4a1503917b9a7a93e1e04ba5954efda3b
Spidgam <yossef.shamalov@gmail.com>
2026-08-02 11:16:54 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    int bac1posx = 0;//background1 prime position x&y
+    int bac1posy = 0;
+
+    int bac2posx = 950;//background2 prime position x&y
+    int bac2posy = 0;
+
+    
+    
+
+    sf::RenderWindow window(sf::VideoMode({960, 540}), "windows mog");
+    window.setFramerateLimit(500);
+    
+    sf::Texture texture("background1.jpg");
+    sf::Sprite sprite(texture);
+    
+    sf::Texture texture2("background2.jpg");
+    sf::Sprite sprite2(texture2);
+    sprite.setPosition({950,0});
+
+    sf::Texture shadowline("shadowline.png");
+    sf::Sprite shadowsprite(shadowline);
+    shadowsprite.setPosition({0,400});
+
+    sf::RectangleShape roadline({960,10});
+    roadline.setFillColor(sf::Color::White);
+    roadline.setPosition({0,400});    
+    
+    while(window.isOpen())
+    {
+        while(const std::optional event  = window.pollEvent())
+        {
+            if(event -> is<sf::Event::Closed>())
+                window.close();
+        }
+        window.clear(sf::Color::Black);
+        if(sprite.getPosition().x==-950)
+        {
+            sprite.setPosition({950,0});
+        }
+        else
+        {
+            sprite.move({-1,0});
+        }
+        if(sprite2.getPosition().x==-950)
+        {
+            sprite2.setPosition({950,0});
+        }
+        else
+        {
+            sprite2.move({-1,0});
+        }
+        
+        window.draw(sprite);
+        window.draw(sprite2);
+        window.draw(shadowsprite);
+        window.draw(roadline);
+        
+
+        window.display();
+    }
+}
